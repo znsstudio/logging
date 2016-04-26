7897b9be3a9a7d13b4b5b41883ab50a711421a0c
Administrator <info@znsstudio.com>
2016-04-26 00:03:21 -0400

    New Functions

---
+++ src/Znsstudio/Logging/Logging.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Znsstudio\Logging;
+
+class Logging
+{
+    public static function init($token)
+    {
+        return $token;
+    }
+
+    public static function send($content,$context="")
+    {
+    	return $content;
+    }
+}
